f19f7c63975ac8b442b8b64254a1ab0c746dfa27
Jardel Back Kuhnen <jardel.kuhnen@mercadolivre.com>
2023-11-02 13:13:24 -0300

    added more endpoints

--- graphql/graph/model/models_gen.go
+++ graphql/graph/model/models_gen.go
@@ -2,27 +2,13 @@
 
 package model
 
-type Category struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Courses     []*Course `json:"courses"`
-}
-
-type Course struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description *string   `json:"description,omitempty"`
-	Category    *Category `json:"category"`
-}
-
 type NewCategory struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description,omitempty"`
 }
 
 type NewCourse struct {
-	Name        string  `json:"name"`
-	Description *string `json:"description,omitempty"`
-	CategoryID  string  `json:"categoryId"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	CategoryID  string `json:"categoryId"`
 }
--- graphql/graph/resolver.go
+++ graphql/graph/resolver.go
@@ -8,4 +8,5 @@ import "github.com/jardelkuhnen/fullcyclecourse/graphql/internal/database"
 
 type Resolver struct {
 	CategoryDB *database.Category
+	CourseDB   *database.Course
 }
